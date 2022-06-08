b84cda649c8c19386e796a5716f526db95d66e8e
Luis Morales <dev.luismorales@gmail.com>
2022-06-08 14:47:02 -0500

    Se realizó la interfaz para mostrar los heroes

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
   exports: [
     MatButtonModule,
+    MatCardModule,
+    MatListModule,
     MatIconModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatListModule
   ]
 })
 export class MaterialModule { }
