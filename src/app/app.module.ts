d82e231176c5a507bca00dbd021c38fe1d4c1424
Luis Morales <dev.luismorales@gmail.com>
2022-06-08 08:51:51 -0500

    implementacion de lazyload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { AppRountingModule } from './app-rounting.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRountingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
