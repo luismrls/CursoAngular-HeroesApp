48b792da7eac95890c5eef04abcef8ea2b897ce2
Luis Morales <dev.luismorales@gmail.com>
2022-06-08 11:49:04 -0500

    se agregaron las imagenes y se termino de estilizar el sidenav

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+
 
 @NgModule({
   exports: [
@@ -11,6 +13,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatIconModule,
     MatSidenavModule,
     MatToolbarModule,
+    MatListModule
   ]
 })
 export class MaterialModule { }
