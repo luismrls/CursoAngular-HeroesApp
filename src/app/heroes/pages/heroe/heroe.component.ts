8d05e35151f7ff4c77937a4bb00328ce2e4f15fa
Luis Morales <dev.luismorales@gmail.com>
2022-06-08 17:01:35 -0500

    se agrego el get para llamar el heroe por id

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs';
+import { Heroe } from '../../interfaces/hereoes.interface';
 import { HeroesService } from '../../services/heroes.service';
 
 @Component({
@@ -11,12 +12,19 @@ import { HeroesService } from '../../services/heroes.service';
 })
 export class HeroeComponent implements OnInit {
 
+  heroe!: Heroe;
+
   constructor(private route: ActivatedRoute, private heroeSevice: HeroesService) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe(({id}) => {
-      console.log(id)
-    });
+    this.route.params
+      .pipe(
+        switchMap(({ id }) => this.heroeSevice.getHeroeById(id))
+      )
+      .subscribe(heroe => {
+        console.log(heroe);
+        this.heroe = heroe;
+      });
   }
 
 }
