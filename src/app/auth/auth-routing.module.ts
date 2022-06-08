da121649318bead5ec08bfebf672a73c420275f3
Luis Morales <dev.luismorales@gmail.com>
2022-06-08 09:23:26 -0500

    se agregó rutas hijas a heroesComponent

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 
