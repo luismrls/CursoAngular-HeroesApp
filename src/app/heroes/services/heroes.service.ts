476960802fe7b27e9ce3f47d79adddc4f8f286ef
Luis Morales <dev.luismorales@gmail.com>
2022-06-09 00:03:16 -0500

    se cambio los url por enviroement

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Heroe } from '../interfaces/hereoes.interface';
 
 @Injectable({
@@ -8,13 +9,15 @@ import { Heroe } from '../interfaces/hereoes.interface';
 })
 export class HeroesService {
 
+  private baseUrl: string = environment.baseUrl;
+
   constructor( private httt: HttpClient) { }
 
   getHeroes(): Observable<Heroe[]> {
-    return this.httt.get<Heroe[]>('http://localhost:3000/heroes')
+    return this.httt.get<Heroe[]>(`${this.baseUrl}/heroes`)
   }
 
   getHeroeById(id: string): Observable<Heroe> {
-    return this.httt.get<Heroe>(`http://localhost:3000/heroes/${id}`)
+    return this.httt.get<Heroe>(`${this.baseUrl}/${id}`)
   }
 }
