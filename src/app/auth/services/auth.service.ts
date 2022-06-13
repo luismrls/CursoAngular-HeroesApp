ab1d8bcf32cd2d929655c6169ecc0068c2bc8d15
Luis Morales <dev.luismorales@gmail.com>
2022-06-13 11:43:31 -0500

    se creo el servicio de auth

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Auth } from '../interfaces/auth.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: String = environment.baseUrl
+
+  constructor(private http: HttpClient) { }
+
+  login(): Observable<Auth> {
+    return this.http.get<Auth>(`${ this.baseUrl }/usuarios/1`)
+  }
+}
