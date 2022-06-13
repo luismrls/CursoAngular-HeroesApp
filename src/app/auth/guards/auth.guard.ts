2430d4771b66121314ac60f85e99dbad05ea513d
Luis Morales <dev.luismorales@gmail.com>
2022-06-13 12:15:57 -0500

    se configuro el CanActive y se guardo el id del usuario en el localStorage

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -6,15 +6,22 @@ import { AuthService } from '../services/auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanLoad {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(private authService: AuthService) {}
 
-  // canActivate(
-  //   route: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-  //   return true;
-  // }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+      if (  this.authService.auth.id ){
+        return true;
+      }
+
+      console.log("Bloqueado por el AuthGuard CanActivate")
+     
+      return false;
+  }
 
   canLoad(
     route: Route,
@@ -24,7 +31,7 @@ export class AuthGuard implements CanLoad {
         return true;
       }
 
-      console.log("Bloqueado por el AuthGuard")
+      console.log("Bloqueado por el AuthGuard - CanLoad")
      
       return false;
   }
