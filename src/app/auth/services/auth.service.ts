f4ecdcc2acfe4e251f6f237bb607e286429e631c
Luis Morales <dev.luismorales@gmail.com>
2022-06-13 11:53:17 -0500

    se muestra el nombre de el usuario al entrar al listado al lado del boton logout

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interface';
 
@@ -10,10 +10,18 @@ import { Auth } from '../interfaces/auth.interface';
 export class AuthService {
 
   private baseUrl: String = environment.baseUrl
+  private _auth: Auth | undefined;
+
+  get auth() {
+    return {...this._auth};
+  }
 
   constructor(private http: HttpClient) { }
 
   login(): Observable<Auth> {
     return this.http.get<Auth>(`${ this.baseUrl }/usuarios/1`)
+      .pipe(
+        tap( auth => this._auth = auth)
+      );
   }
 }
